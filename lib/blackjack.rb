c8863861163f449361eea5ee4b84542aca1d336a
Alex Suthammanont <asuthamm@gmail.com>
2019-11-11 19:18:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,16 +55,16 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-def runner
-  card_total = 0
-  welcome
-  until card_total > 21 do
+# def runner
+#   card_total = 0
+#   welcome
+#   until card_total > 21 do
 
-    card_total = hit?(initial_round)
-    puts card_total
-    if card_total > 21
-      puts "you loose"
-    end
-  end
-end
+#     card_total = hit?(initial_round)
+#     puts card_total
+#     if card_total > 21
+#       puts "you loose"
+#     end
+#   end
+# end
     
