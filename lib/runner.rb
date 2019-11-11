6596c6268e0ff831b78ec657aef005ffb57c8510
Alex Suthammanont <asuthamm@gmail.com>
2019-11-11 19:18:13 +0000

    Automatically backed up by Learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,14 @@
 require_relative "blackjack.rb"
 
-runner
+def runner
+  card_total = 0
+  welcome
+  until card_total > 21 do
+
+    card_total = hit?(initial_round)
+    puts card_total
+    if card_total > 21
+      puts "you loose"
+    end
+  end
+end
